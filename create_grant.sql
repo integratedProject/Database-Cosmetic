9326aaa2afc109e4e5597824d16cdc43de869e42
Ubuntu <Powerpuffgirl@Cosmetic.32lxvgqbnqaelfe15ug1kovw5e.ix.internal.cloudapp.net>
2021-05-09 16:30:12 +0000

    Edit file create_grant file

--- create_grant.sql
+++ create_grant.sql
@@ -134,4 +134,6 @@ UNLOCK TABLES;
 -- Dump completed on 2021-05-05 16:32:56
 create user 'powerpuffgirl'@'172.30.0.4' identified by 'powerpuffgirl1234';
 grant select,update,delete,insert on cosmetic.product to 'powerpuffgirl'@'172.30.0.4';
+grant select on cosmetic.color to 'powerpuffgirl'@'172.30.0.4';
+grant select on cosmetic.brand to 'powerpuffgirl'@'172.30.0.4';
 
